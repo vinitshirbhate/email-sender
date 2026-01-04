63b9c597e2cb6722c733217753634c94c565ba94
vinitshirbhate <vinit.shirbhate23@vit.edu>
2026-01-04 23:44:25 +0530

    done

--- consumer.go
+++ consumer.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/smtp"
 	"sync"
+	"time"
 )
 
 
 func emailWorker(ch chan Recipient,id int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for recipient := range ch {
+		smtpHost:="localhost"
+		smtpPort:="1025"
+
+		// formatingMsg := fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s",
+		// 	"vinit@gmail.com",
+		// 	recipient.Email,
+		// 	"testing")
+		// msg:= []byte(formatingMsg)
+
+		msg,err:=executeTemplate(recipient)
+		if err != nil{
+			log.Fatal(err)
+		}
+		err = smtp.SendMail(smtpHost+":"+smtpPort,nil,"vinit@gmail.com",[]string{recipient.Email},[]byte(msg)) 
+		if err != nil{
+			log.Fatal(err)
+		}
+
+		time.Sleep(50*time.Millisecond)
 		fmt.Println(id,recipient)
 	}
 }
--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "sync"
+import (
+	"bytes"
+	"sync"
+	"text/template"
+)
 
 type Recipient struct {
 	Name string
@@ -20,4 +24,19 @@ func main() {
 	}
 
 	wg.Wait()
+}
+
+
+func executeTemplate(r Recipient) (string, error) {
+	t,err := template.ParseFiles("email.tmpl")
+	if err != nil {
+		return "", err
+	}
+	var tpl bytes.Buffer
+	t.Execute(&tpl,r)
+
+	if err != nil {
+		return "", err
+	}
+	return tpl.String(), nil
 }
